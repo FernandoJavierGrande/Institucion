e87539629624cf9c5a109698b6dd7a323150387d
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2022-11-07 09:25:19 -0300

    Vista

--- Institucion/FormInstitucion.Designer.cs
+++ Institucion/FormInstitucion.Designer.cs
@@ -28,12 +28,340 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNombreM = new System.Windows.Forms.TextBox();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.DgvAm = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtApellido = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TxtTurno = new System.Windows.Forms.TextBox();
+            this.TxtDni = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.TxtFechaInicio = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.RbSi = new System.Windows.Forms.RadioButton();
+            this.RbNo = new System.Windows.Forms.RadioButton();
+            this.BtGuardarM = new System.Windows.Forms.Button();
+            this.BtCambiarM = new System.Windows.Forms.Button();
+            this.BtEliminarM = new System.Windows.Forms.Button();
+            this.BtGuardarA = new System.Windows.Forms.Button();
+            this.BtCambiarA = new System.Windows.Forms.Button();
+            this.BtEliminarA = new System.Windows.Forms.Button();
+            this.BtConfirmarCambios = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAm)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(134, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Datos Materia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(134, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(128, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Datos Alumno";
+            // 
+            // TxtNombreM
+            // 
+            this.TxtNombreM.Location = new System.Drawing.Point(138, 58);
+            this.TxtNombreM.Name = "TxtNombreM";
+            this.TxtNombreM.Size = new System.Drawing.Size(100, 20);
+            this.TxtNombreM.TabIndex = 2;
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(138, 219);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(100, 20);
+            this.TxtNombre.TabIndex = 3;
+            // 
+            // DgvAm
+            // 
+            this.DgvAm.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DgvAm.Location = new System.Drawing.Point(365, 22);
+            this.DgvAm.Name = "DgvAm";
+            this.DgvAm.Size = new System.Drawing.Size(391, 387);
+            this.DgvAm.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Nombre";
+            // 
+            // TxtApellido
+            // 
+            this.TxtApellido.Location = new System.Drawing.Point(138, 256);
+            this.TxtApellido.Name = "TxtApellido";
+            this.TxtApellido.Size = new System.Drawing.Size(100, 20);
+            this.TxtApellido.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 259);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Apellido";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(67, 92);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Turno";
+            // 
+            // TxtTurno
+            // 
+            this.TxtTurno.Location = new System.Drawing.Point(138, 92);
+            this.TxtTurno.Name = "TxtTurno";
+            this.TxtTurno.Size = new System.Drawing.Size(100, 20);
+            this.TxtTurno.TabIndex = 10;
+            // 
+            // TxtDni
+            // 
+            this.TxtDni.Location = new System.Drawing.Point(138, 299);
+            this.TxtDni.Name = "TxtDni";
+            this.TxtDni.Size = new System.Drawing.Size(100, 20);
+            this.TxtDni.TabIndex = 11;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(67, 306);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(32, 13);
+            this.label.TabIndex = 12;
+            this.label.Text = "D.N.I";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(67, 345);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Edad";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 342);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(67, 385);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "FechaInicio";
+            // 
+            // TxtFechaInicio
+            // 
+            this.TxtFechaInicio.Location = new System.Drawing.Point(138, 385);
+            this.TxtFechaInicio.Name = "TxtFechaInicio";
+            this.TxtFechaInicio.Size = new System.Drawing.Size(100, 20);
+            this.TxtFechaInicio.TabIndex = 16;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(67, 130);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(41, 13);
+            this.label9.TabIndex = 17;
+            this.label9.Text = "Aprobo";
+            // 
+            // RbSi
+            // 
+            this.RbSi.AutoSize = true;
+            this.RbSi.Location = new System.Drawing.Point(138, 130);
+            this.RbSi.Name = "RbSi";
+            this.RbSi.Size = new System.Drawing.Size(34, 17);
+            this.RbSi.TabIndex = 18;
+            this.RbSi.TabStop = true;
+            this.RbSi.Text = "Si";
+            this.RbSi.UseVisualStyleBackColor = true;
+            // 
+            // RbNo
+            // 
+            this.RbNo.AutoSize = true;
+            this.RbNo.Location = new System.Drawing.Point(138, 153);
+            this.RbNo.Name = "RbNo";
+            this.RbNo.Size = new System.Drawing.Size(39, 17);
+            this.RbNo.TabIndex = 19;
+            this.RbNo.TabStop = true;
+            this.RbNo.Text = "No";
+            this.RbNo.UseVisualStyleBackColor = true;
+            // 
+            // BtGuardarM
+            // 
+            this.BtGuardarM.Location = new System.Drawing.Point(268, 58);
+            this.BtGuardarM.Name = "BtGuardarM";
+            this.BtGuardarM.Size = new System.Drawing.Size(75, 23);
+            this.BtGuardarM.TabIndex = 20;
+            this.BtGuardarM.Text = "Guardar";
+            this.BtGuardarM.UseVisualStyleBackColor = true;
+            // 
+            // BtCambiarM
+            // 
+            this.BtCambiarM.Location = new System.Drawing.Point(268, 104);
+            this.BtCambiarM.Name = "BtCambiarM";
+            this.BtCambiarM.Size = new System.Drawing.Size(75, 23);
+            this.BtCambiarM.TabIndex = 21;
+            this.BtCambiarM.Text = "Cambiar";
+            this.BtCambiarM.UseVisualStyleBackColor = true;
+            // 
+            // BtEliminarM
+            // 
+            this.BtEliminarM.Location = new System.Drawing.Point(268, 147);
+            this.BtEliminarM.Name = "BtEliminarM";
+            this.BtEliminarM.Size = new System.Drawing.Size(75, 23);
+            this.BtEliminarM.TabIndex = 22;
+            this.BtEliminarM.Text = "Eliminar";
+            this.BtEliminarM.UseVisualStyleBackColor = true;
+            // 
+            // BtGuardarA
+            // 
+            this.BtGuardarA.Location = new System.Drawing.Point(268, 217);
+            this.BtGuardarA.Name = "BtGuardarA";
+            this.BtGuardarA.Size = new System.Drawing.Size(75, 23);
+            this.BtGuardarA.TabIndex = 23;
+            this.BtGuardarA.Text = "Guardar";
+            this.BtGuardarA.UseVisualStyleBackColor = true;
+            // 
+            // BtCambiarA
+            // 
+            this.BtCambiarA.Location = new System.Drawing.Point(268, 259);
+            this.BtCambiarA.Name = "BtCambiarA";
+            this.BtCambiarA.Size = new System.Drawing.Size(75, 23);
+            this.BtCambiarA.TabIndex = 24;
+            this.BtCambiarA.Text = "Cambiar";
+            this.BtCambiarA.UseVisualStyleBackColor = true;
+            // 
+            // BtEliminarA
+            // 
+            this.BtEliminarA.Location = new System.Drawing.Point(268, 301);
+            this.BtEliminarA.Name = "BtEliminarA";
+            this.BtEliminarA.Size = new System.Drawing.Size(75, 23);
+            this.BtEliminarA.TabIndex = 25;
+            this.BtEliminarA.Text = "Eliminar";
+            this.BtEliminarA.UseVisualStyleBackColor = true;
+            // 
+            // BtConfirmarCambios
+            // 
+            this.BtConfirmarCambios.Location = new System.Drawing.Point(241, 415);
+            this.BtConfirmarCambios.Name = "BtConfirmarCambios";
+            this.BtConfirmarCambios.Size = new System.Drawing.Size(102, 23);
+            this.BtConfirmarCambios.TabIndex = 26;
+            this.BtConfirmarCambios.Text = "Confirmar Cambios";
+            this.BtConfirmarCambios.UseVisualStyleBackColor = true;
+            // 
+            // FormInstitucion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtConfirmarCambios);
+            this.Controls.Add(this.BtEliminarA);
+            this.Controls.Add(this.BtCambiarA);
+            this.Controls.Add(this.BtGuardarA);
+            this.Controls.Add(this.BtEliminarM);
+            this.Controls.Add(this.BtCambiarM);
+            this.Controls.Add(this.BtGuardarM);
+            this.Controls.Add(this.RbNo);
+            this.Controls.Add(this.RbSi);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.TxtFechaInicio);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.TxtDni);
+            this.Controls.Add(this.TxtTurno);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtApellido);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.DgvAm);
+            this.Controls.Add(this.TxtNombre);
+            this.Controls.Add(this.TxtNombreM);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormInstitucion";
             this.Text = "FormInstitucion";
+            ((System.ComponentModel.ISupportInitialize)(this.DgvAm)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtNombreM;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.DataGridView DgvAm;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtApellido;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox TxtTurno;
+        private System.Windows.Forms.TextBox TxtDni;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox TxtFechaInicio;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.RadioButton RbSi;
+        private System.Windows.Forms.RadioButton RbNo;
+        private System.Windows.Forms.Button BtGuardarM;
+        private System.Windows.Forms.Button BtCambiarM;
+        private System.Windows.Forms.Button BtEliminarM;
+        private System.Windows.Forms.Button BtGuardarA;
+        private System.Windows.Forms.Button BtCambiarA;
+        private System.Windows.Forms.Button BtEliminarA;
+        private System.Windows.Forms.Button BtConfirmarCambios;
     }
 }
