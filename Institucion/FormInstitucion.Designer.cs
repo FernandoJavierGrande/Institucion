af0b1d9dda496b619e85d9ba5e97b716bd7c671d
MarianoProgramador <marian_cejas_93@hotmail.com.ar>
2022-11-07 12:18:33 -0300

    Vista

--- Institucion/FormInstitucion.Designer.cs
+++ Institucion/FormInstitucion.Designer.cs
@@ -55,13 +55,14 @@
             this.BtCambiarA = new System.Windows.Forms.Button();
             this.BtEliminarA = new System.Windows.Forms.Button();
             this.BtConfirmarCambios = new System.Windows.Forms.Button();
+            this.BtSalir = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DgvAm)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.BackColor = System.Drawing.Color.Cyan;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(134, 22);
             this.label1.Name = "label1";
@@ -293,12 +294,22 @@
             this.BtConfirmarCambios.Text = "Confirmar Cambios";
             this.BtConfirmarCambios.UseVisualStyleBackColor = true;
             // 
+            // BtSalir
+            // 
+            this.BtSalir.Location = new System.Drawing.Point(686, 415);
+            this.BtSalir.Name = "BtSalir";
+            this.BtSalir.Size = new System.Drawing.Size(102, 23);
+            this.BtSalir.TabIndex = 27;
+            this.BtSalir.Text = "Salir";
+            this.BtSalir.UseVisualStyleBackColor = true;
+            // 
             // FormInstitucion
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Lime;
+            this.BackColor = System.Drawing.Color.Cyan;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtSalir);
             this.Controls.Add(this.BtConfirmarCambios);
             this.Controls.Add(this.BtEliminarA);
             this.Controls.Add(this.BtCambiarA);
@@ -363,5 +374,6 @@
         private System.Windows.Forms.Button BtCambiarA;
         private System.Windows.Forms.Button BtEliminarA;
         private System.Windows.Forms.Button BtConfirmarCambios;
+        private System.Windows.Forms.Button BtSalir;
     }
 }
